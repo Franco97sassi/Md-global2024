4e65d7722941ad4107057c59950a70a97ff06c03
marcosferr1 <marcos.e.ferr@gmail.com>
2024-01-04 18:09:22 -0300

    material ui

--- frontend/mdglobal2024/src/componentes/Servicios/Servicios.jsx
+++ frontend/mdglobal2024/src/componentes/Servicios/Servicios.jsx
@@ -9,8 +9,7 @@ import Permisos from './Permisos';
 import Atencion from './Atencion';
 import Despacho from './Despacho';
 import Auditoria from './Auditoria';
-
- // Asegúrate de tener un archivo de estilo CSS para estilar tu carrusel.
+import flechita from '../../assets/servicios/flechita.png';
 
 function Servicios() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -18,13 +17,12 @@ function Servicios() {
   const slides = [
     <Warehousing key={1} />,
     <Logistica key={2} />,
-    <Arancelaria key={3}/>,
+    <Arancelaria key={3} />,
     <Seguridad key={4}></Seguridad>,
     <Permisos key={5}></Permisos>,
     <Atencion key={6}></Atencion>,
     <Despacho key={7}></Despacho>,
     <Auditoria key={8}></Auditoria>
-
   ];
 
   const prevSlide = () => {
@@ -41,19 +39,22 @@ function Servicios() {
         <Navbar></Navbar>
       </header>
       <main>
-        <div className="carousel-container" style={{marginTop: '10%' , marginBottom: '5%' , marginLeft: '5%'}}>
+        <div className="carousel-container" style={{ marginTop: '10%', marginBottom: '5%', marginLeft: '5%' }}>
           <div className="carousel">
-            <div  className="slide-container">
-            <button onClick={prevSlide}>Previous</button>
-      
-            {slides[currentSlide]}
-            <button onClick={nextSlide}>Next</button>
+            <div className="slide-container" style={{ display: 'flex', flexDirection: 'row'  , alignContent: 'center' , justifyContent: 'center'}}>
+              <button className='flechitaLeft' style={{border: '0px' ,margin: '20px' , height: 'fit-content' , alignSelf: 'center'}} onClick={prevSlide}>
+                <img src={flechita} alt="Previous" />
+              </button>
+
+              {slides[currentSlide]}
+
+              <button className='flechitaRight' style={{border: '0px' , margin: '20px', transform: 'rotate(180deg)' , alignSelf: 'center', height: 'fit-content'}} onClick={nextSlide}>
+                <img src={flechita} alt="Next" />
+              </button>
             </div>
           </div>
 
-          <div className="controls">
-
-          </div>
+          <div className="controls"></div>
         </div>
       </main>
       <footer>

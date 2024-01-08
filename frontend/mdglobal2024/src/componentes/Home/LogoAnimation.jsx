968cf11b636de38d23ace63fc16b0165baa6649a
marcosferr1 <marcos.e.ferr@gmail.com>
2024-01-08 14:04:52 -0300

    a

--- frontend/mdglobal2024/src/componentes/Home/LogoAnimation.jsx
+++ frontend/mdglobal2024/src/componentes/Home/LogoAnimation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './LogoAnimation.css';
+import './logoAnimation.css'
 import logo from '../../assets/Home/logoInicio.png'
 import { isMobile } from 'react-device-detect';
 
